1dbdc8e9064c6e3946d48c61a6a45e478e14e03e
Marina-D-G <147334873+Marina-D-G@users.noreply.github.com>
2024-05-19 13:49:01 +0300

    Create Person.h

---
+++ Question 10/Person, Student, Teacher/Person/Person.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+
+class Person
+{
+private:
+	char* name = nullptr;
+	int age = 0;
+
+	void copyFrom(const Person& other);
+	void free();
+	void moveFrom(Person&& other);
+
+public:
+	Person() = default;
+	Person(const char* name, int age);
+
+	Person(const Person& other);
+	Person& operator=(const Person& other);
+
+	Person(Person&& other);
+	Person& operator=(Person&& other);
+
+	const char* getName() const;
+	int getAge() const;
+
+	~Person();
+	void print() const;
+
+protected:
+	void setName(const char* name);
+	void setAge(int age);
+};
