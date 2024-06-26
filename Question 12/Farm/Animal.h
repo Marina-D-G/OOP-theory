e927a97a59027c379f028674ff6bbc41e7f01f48
Marina-D-G <147334873+Marina-D-G@users.noreply.github.com>
2024-06-26 15:28:19 +0300

    Create Animal.h

---
+++ Question 12/Farm/Animal.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+
+enum class AnimalType
+{
+	Dog,
+	Cat,
+	Cow
+};
+
+class Animal
+{
+public:
+	Animal(AnimalType type) : type(type) {};
+	virtual void roar() const = 0;
+	virtual Animal* clone()  const = 0;//създава копие на текущия обект.
+	virtual ~Animal() = default;
+
+	AnimalType getType() const 
+	{
+		return type;
+	}
+private:
+	AnimalType type;
+};
