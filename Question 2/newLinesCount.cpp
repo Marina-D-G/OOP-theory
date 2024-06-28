d6671d20babe05ed1cfa12d56ade3d44b17a7dc6
Marina-D-G <147334873+Marina-D-G@users.noreply.github.com>
2024-06-28 22:51:52 +0300

    Update newLinesCount.cpp

--- Question 2/newLinesCount.cpp
+++ Question 2/newLinesCount.cpp
@@ -4,39 +4,45 @@
 
 const char NEW_LINE_CHAR = '\n';
 
-int getCharCount(std::ifstream& ifs, char ch) {
-
-	if (!ifs.is_open()) {
+int getCharCount(std::ifstream& ifs, char ch) 
+{
+	if (!ifs.is_open()) 
+	{
 		return -1;
 	}
 
 	int count = 0;
-	while (true) {
+	while (true) 
+	{
 		char current = ifs.get();
-
-		if (ifs.eof()) {
-			return count;
+		if (ifs.eof()) 
+		{
+			return count; // ако сме достигнали края на файла, връщаме брояча
 		}
 
-		if (current == ch) {
-			count++;
+		if (current == ch) 
+		{
+			count++; //ако символа е нашият, увеличаваме брояча
 		}
 	}
-	return 0;
 }
 
-int getLinesCount(const char* fileName) {
-	if (!fileName) {
+int getLinesCount(const char* fileName)
+{
+	if (!fileName) 
+	{
 		return -1;
 	}
 
 	std::ifstream ifs(fileName);
-	if (!ifs.is_open()) {
+	if (!ifs.is_open()) 
+	{
 		return -1;
 	}
 
-	return getCharCount(ifs, NEW_LINE_CHAR) + 1;
+	return getCharCount(ifs, NEW_LINE_CHAR) + 1; // броят на новите редове е броят на \n символите + 1
 }
+
 int main()
 {
 	std::cout << getLinesCount("file.txt");
