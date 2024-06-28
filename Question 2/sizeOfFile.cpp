0f4a4695ab589b9fa62fa6c8b995e0e9a5c608ef
Marina-D-G <147334873+Marina-D-G@users.noreply.github.com>
2024-06-28 22:46:19 +0300

    Update sizeOfFile.cpp

--- Question 2/sizeOfFile.cpp
+++ Question 2/sizeOfFile.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <fstream>
 
-size_t getFileSize(const std::string& fileName) 
+size_t getFileSize(const char* fileName) 
 {
     std::ifstream file(fileName);
     if (file.is_open()) 
     {
-        size_t current = file.tellg();
-	      file.seekg(0, std::ios::end);
-	      size_t size = file.tellg();
-	      file.seekg(current, std::ios::beg);
+        size_t current = file.tellg(); // пази началната позиция на get указателя (за да можем да си я върнем после)
+	file.seekg(0, std::ios::end); // мести го в края
+	size_t size = file.tellg(); // взема позицията, която вече е в края (това е размерът на файла)
+	file.seekg(current, std::ios::beg); // връщаме го в началото на файла
+
+	return size;
     }
     else 
     {
-        return -1;
+        return -1; // ако файлът не е отворен, връщаме -1 за размер
     }
 }
